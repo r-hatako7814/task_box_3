8b56e28b0781c17a960e4c8051c6aca15eba653c
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-09-27 15:16:03 +0000

    9/27

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!,except: [:top,:about]
+  before_action :authenticate_user!,except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
-   def after_sign_in_path_for(resource)
-    user_path(resource)
-   end
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   root to:'homes#top'
   get  'home/about'=>'homes#about'
 
-  resources :books,only:[:new,:create,:index,:show,:destroy,:edit,:update]
+  resources :books,only:[:new,:create,:index,:show,:destroy,:edit,:update] do
+    resource :favorites, only:[:create, :destroy]
 
-
+    resources :book_comments, only: [:create,:destroy]
+  end
   resources:users,only:[:show,:edit,:update,:index]
-
 end
