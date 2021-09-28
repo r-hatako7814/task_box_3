8fa8a82407d80d2890b3676f882d403a5d04def0
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-09-28 07:47:19 +0000

    9/28

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,7 +5,9 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+  　redirect_back(fallback_location: root_path)
+
+
   end
 
   def destroy
